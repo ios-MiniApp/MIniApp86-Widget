1f87bde63030dfd1f86792eae2d5f28db9d4fbbe
Kota <maedakoutaa@gmail.com>
2022-08-15 11:03:38 +0900

    Initial Commit

---
+++ MIniApp86-Widget/MIniApp86_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIniApp86_WidgetApp.swift
+//  MIniApp86-Widget
+//
+//  Created by 前田航汰 on 2022/08/15.
+//
+
+import SwiftUI
+
+@main
+struct MIniApp86_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
